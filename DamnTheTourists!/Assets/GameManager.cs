5ba8c5c4cd177e42a7d51053b5a820d0274300ad
LosTacosDeZelda <jeudyraphael@gmail.com>
2021-11-14 20:05:46 -0500

    Stopwatch

---
+++ DamnTheTourists!/Assets/GameManager.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class GameManager : MonoBehaviour
+{
+
+    float stopwatch = 0;
+    public TMP_Text stopwatchText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        stopwatch +=  Time.deltaTime;
+
+        if (stopwatch > 59)
+        {
+            if (stopwatch % 60 == 0)
+            {
+                stopwatchText.text = "Time : " + (Mathf.RoundToInt(stopwatch) / 60).ToString() + "min ";
+            }
+            else
+            {
+                stopwatchText.text = "Time : " + (Mathf.RoundToInt(stopwatch) / 60).ToString() + "min " + Mathf.RoundToInt(stopwatch % 60).ToString() + " sec";
+            }
+           
+        }
+        else
+        {
+            stopwatchText.text = "Time : " + Mathf.RoundToInt(stopwatch).ToString() + " sec";
+        }
+        
+    }
+
+    void IncrementTime()
+    {
+
+    }
+}
